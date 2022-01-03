7869c3e269e9a0b939c83de12b1560327aa72b2d
Anu-Trivedi <42116521+Anu-Trivedi@users.noreply.github.com>
2022-01-03 13:10:57 -0800

    first commit

--- app/__init__.py
+++ app/__init__.py
@@ -28,6 +28,10 @@ def create_app(config_class=Config):
     def not_found(e):
         return app.send_static_file('index.html')
 
+    @app.route("/favicon.ico")
+    def favicon():
+        return "", 200
+
     from .blueprints.auth import bp as auth_bp
     app.register_blueprint(auth_bp)
 
