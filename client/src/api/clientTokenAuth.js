dfb03d5845e8367df66e04d45d9f83f3b8011ad3
Anu-Trivedi <42116521+Anu-Trivedi@users.noreply.github.com>
2022-01-03 14:02:04 -0800

    initial commit

--- client/src/api/clientTokenAuth.js
+++ client/src/api/clientTokenAuth.js
@@ -2,7 +2,7 @@ import {create} from "apisauce";
 
 const apiClientWithToken = (token) => create(
     {
-        baseURL: "http://127.0.0.1:5000",
+        baseURL: window.location.hostname === 'localhost'|| window.location.hostname === '127.0.0.1' ? "http://127.0.0.1:5000" : '',
         headers:{
             Authorization: "Bearer " + token
         }
