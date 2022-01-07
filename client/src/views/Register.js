e370377af2f83918160f258c56ee9e12cda99031
Anu-Trivedi <42116521+Anu-Trivedi@users.noreply.github.com>
2022-01-07 09:57:09 -0800

    updating api call

--- client/src/views/Register.js
+++ client/src/views/Register.js
@@ -30,8 +30,7 @@ export default class Register extends Component {
     }
     
     handleSubmit = ({firstname, lastname, username, password}) => {
-        // axios.post(`http://127.0.0.1:5000/user`, {
-        axios.post(`https://exp-tracker-tool.herokuapp.com/user`, {
+         axios.post(`http://127.0.0.1:5000/user`, {
             firstname:firstname,
             lastname:lastname,
             username:username,
