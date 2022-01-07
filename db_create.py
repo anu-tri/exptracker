33d0e5ac53dde09d10d911b58df1208e0bf10d70
Anu-Trivedi <42116521+Anu-Trivedi@users.noreply.github.com>
2022-01-06 23:58:25 -0800

    changing to database_url

--- db_create.py
+++ db_create.py
@@ -1,3 +1,11 @@
-from app import db
+import click
+from flask.cli import with_appcontext
 
-db.create_all()
+from flask_sqlalchemy import SQLAlchemy
+from app.models import User, Category, Income, Expense
+
+db = SQLAlchemy()
+@click.command(name='create_tables')
+# @with_appcontext
+def create_tables():
+    db.create_all()
