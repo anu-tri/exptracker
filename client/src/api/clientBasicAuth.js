66ff0bb304d75ee7ca93940ee7bea8c6713494a6
Anu-Trivedi <42116521+Anu-Trivedi@users.noreply.github.com>
2022-01-07 11:58:27 -0800

    modified axios api call

--- client/src/api/clientBasicAuth.js
+++ client/src/api/clientBasicAuth.js
@@ -5,7 +5,7 @@ import base64 from "base-64";
 const apiClient = (username, password) => create(
     {
         // baseURL: `https://expense-tracker-tool.herokuapp.com`,
-        baseURL:process.env.baseURL || "http://127.0.0.1:5000",
+        // baseURL:process.env.baseURL || "http://127.0.0.1:5000",
         headers:{
             Authorization: "Basic " + base64.encode(username+":"+password)
     }
