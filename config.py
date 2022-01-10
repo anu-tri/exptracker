41ec097e23bd1c4babb7f74585fec1444e3ef896
Anu-Trivedi <42116521+Anu-Trivedi@users.noreply.github.com>
2022-01-09 21:58:04 -0800

    modified register

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ class Config():
     SECRET_KEY = os.environ.get("SECRET_KEY") or "placeholder{%you-will-never-guess%}placeholder"
     SQLALCHEMY_DATABASE_URI= os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS= os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
+    HOST_ADDRESS=os.environ.get('HOST_ADDRESS')
