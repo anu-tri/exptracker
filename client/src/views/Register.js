f32aa5d9e1f2c69ce219e5193f67b5c3e2a8f061
Anu-Trivedi <42116521+Anu-Trivedi@users.noreply.github.com>
2022-01-10 12:57:35 -0800

    static.json

--- client/src/views/Register.js
+++ client/src/views/Register.js
@@ -28,27 +28,31 @@ export default class Register extends Component {
             redirect: false
         };
     }
+
+    componentDidMount(){
+        console.log("in registerr:"+ window.location.hostname )
+    }
     
     handleSubmit = ({firstname, lastname, username, password}) => {
         // let baseURL = process.env.REACT_APP_BASE_URL;
         // axios.post(`${window.location.host}/user`, {
-        console.log("in registerr:"+ window.location.hostname )
-        // axios.post("https://expense-tracker-tool.herokuapp.com/user", {
-        //     firstname:firstname,
-        //     lastname:lastname,
-        //     username:username,
-        //     password:password
-        // })
-        // .then(response=>{
-        //     if (response.data){
-        //         console.log(response.data);
-        //         this.setState({redirect:true});
-        //     }
-        // })
-        // .catch(error=>{
-        //     this.setState({error:response.error});
-        //     console.log('Error creating user: ', error);
-        // })
+        
+        axios.post("https://expense-tracker-tool.herokuapp.com/user", {
+            firstname:firstname,
+            lastname:lastname,
+            username:username,
+            password:password
+        })
+        .then(response=>{
+            if (response.data){
+                console.log(response.data);
+                this.setState({redirect:true});
+            }
+        })
+        .catch(error=>{
+            this.setState({error:response.error});
+            console.log('Error creating user: ', error);
+        })
     }
 
 
