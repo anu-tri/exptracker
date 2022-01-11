9c9231cbd5457f9b837db9426062898b3cad857a
Anu-Trivedi <42116521+Anu-Trivedi@users.noreply.github.com>
2022-01-11 10:18:49 -0800

    finally

--- app/__init__.py
+++ app/__init__.py
@@ -4,24 +4,21 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_moment import Moment 
-from flask_cors import CORS
+# from flask_cors import CORS
 
 
 db = SQLAlchemy()
 migrate = Migrate()
 moment = Moment()
-cors = CORS()
+# cors = CORS()
 
 def create_app(config_class=Config):
     app = Flask(__name__, static_folder="../client/build", static_url_path='')
     app.config.from_object(config_class)
-    #register plugins
-    # login.init_app(app)    
     db.init_app(app)
     migrate.init_app(app,db)
     moment.init_app(app)
-    cors.init_app(app)
-
+    # cors.init_app(app)
 
 
     @app.route('/')
@@ -32,9 +29,9 @@ def create_app(config_class=Config):
     def not_found(error):
         return app.send_static_file('index.html')
 
-    @app.route("/favicon.ico")
-    def favicon():
-      return "", 200
+    # @app.route("/favicon.ico")
+    # def favicon():
+    #   return "", 200
 
     from .blueprints.auth import bp as auth_bp
     app.register_blueprint(auth_bp)
