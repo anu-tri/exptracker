a926191698f271562c5ef73d1f5476c8bc0d8dde
Anu-Trivedi <42116521+Anu-Trivedi@users.noreply.github.com>
2022-01-10 19:56:40 -0800

    test

--- client/src/views/Register.js
+++ client/src/views/Register.js
@@ -29,10 +29,7 @@ export default class Register extends Component {
         };
     }
 
-    componentDidMount(){
-        console.log("in registerr:"+ window.location.hostname )
-    }
-    
+       
     handleSubmit = ({firstname, lastname, username, password}) => {
         // let baseURL = process.env.REACT_APP_BASE_URL;
         // axios.post(`${window.location.host}/user`, {
@@ -84,7 +81,7 @@ export default class Register extends Component {
                         {({ errors, touched }) => (
                             <Form style={{backgroundColor:"purple", width:"30%", borderRadius:"10px", marginTop:"50px", marginLeft:"380px"}}>
                                 <label htmlFor="firstname" className="form-label" style={styles.text}>Firstname</label>
-                                <Field name="firstname" style={styles.field} />
+                                <Field name="firstname" style={styles.field} text={window.location.hostname} />
                                 {errors.firstname && touched.firstname ? (
                                     <div style={styles.error}>{errors.firstname}</div>
                                 ) : null}
